40e57e7101aa1b8f89eeb12c826b456f85e26cb7
Bharath Kumar <pbkbharath1707@gmail.com>
2020-08-02 21:47:28 +0530

    Updates

---
+++ precache-manifest.3a3e98a7e32530f7650eea31b462d036.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c326ca3012e85ce3bc68686a97e9e7b0",
+    "url": "/Netflix-Bharath/index.html"
+  },
+  {
+    "revision": "620e003831965d702cb2",
+    "url": "/Netflix-Bharath/static/css/main.2d28a711.chunk.css"
+  },
+  {
+    "revision": "409ff5e8420c7b454c4b",
+    "url": "/Netflix-Bharath/static/js/2.b8f57773.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-Bharath/static/js/2.b8f57773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620e003831965d702cb2",
+    "url": "/Netflix-Bharath/static/js/main.1de3f9f8.chunk.js"
+  },
+  {
+    "revision": "ca67cea08686550976e5",
+    "url": "/Netflix-Bharath/static/js/runtime-main.492d5a52.js"
+  }
+]);
